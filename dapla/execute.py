b7995dfad40c301865081666b9e125046d15e062
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2020-09-10 22:12:33 +0200

    Improve error handling for expired sessions

--- dapla/execute.py
+++ dapla/execute.py
@@ -1,23 +1,32 @@
 import os
 
-from dapla.jupyterextensions.authextension import AuthClient
+from dapla.jupyterextensions.authextension import AuthClient, AuthError
 from dapla.services.reader import DataSourceReader
 from dapla.services.writer import DataSourceWriter
 
 
 def read_pandas(path):
     reader = DataSourceReader(AuthClient.get_access_token, os.environ['DATA_ACCESS_URL'],  os.environ['CATALOG_URL'])
-    return reader.read(path)
+    try:
+        return reader.read(path)
+    except AuthError as err:
+        err.print_warning()
 
 
 def show(path):
     reader = DataSourceReader(AuthClient.get_access_token, os.environ['DATA_ACCESS_URL'],  os.environ['CATALOG_URL'])
-    return reader.list(path)
+    try:
+        return reader.list(path)
+    except AuthError as err:
+        err.print_warning()
 
 
 def details(path):
     reader = DataSourceReader(AuthClient.get_access_token, os.environ['DATA_ACCESS_URL'],  os.environ['CATALOG_URL'])
-    return reader.details(path)
+    try:
+        return reader.details(path)
+    except AuthError as err:
+        err.print_warning()
 
 
 def write_pandas(df, path, **option_kwargs):
@@ -29,4 +38,7 @@ def write_pandas(df, path, **option_kwargs):
         os.environ['METADATA_PUBLISHER_TOPIC_NAME'],
         os.environ['CATALOG_URL']
     )
-    writer.write(df, path, **option_kwargs)
+    try:
+        writer.write(df, path, **option_kwargs)
+    except AuthError as err:
+        err.print_warning()
