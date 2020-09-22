9802f2905c79e9e677348955b07c017a55e9e549
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2020-09-22 17:50:57 +0200

    Load lineage magic by default

--- dapla/magics/__init__.py
+++ dapla/magics/__init__.py
@@ -5,3 +5,5 @@ from .lineage import DaplaLineageMagics
 def load_all(ipython):
     from .documentation import load_ipython_extension
     load_ipython_extension(ipython)
+    from .lineage import load_ipython_extension
+    load_ipython_extension(ipython)
