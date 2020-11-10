b3cdb01ba81cf399f5852fd45ca8c6bea357f05c
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2020-11-10 14:58:51 +0100

    Bump version: 0.2.11 → 0.2.12

--- dapla/__init__.py
+++ dapla/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.11'
+__version__ = '0.2.12'
 __author__ = 'Statistics Norway (ssb.no)'
 __all__ = []
 
--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ DEPENDENCIES = [
 
 setup(
     name="ssb-ipython-kernels",
-    version="0.2.11",
+    version="0.2.12",
     url="https://github.com/statisticsnorway/dapla-ipython-kernels",
     license='MIT',
 
