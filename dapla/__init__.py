fa29fd690308d51be4320fd95c7968e1e0573bf7
RuneLind <rulind@gmail.com>
2020-11-16 11:20:49 +0100

    Bump version: 0.2.12 → 0.2.13

--- dapla/__init__.py
+++ dapla/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.12'
+__version__ = '0.2.13'
 __author__ = 'Statistics Norway (ssb.no)'
 __all__ = []
 
--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ DEPENDENCIES = [
 
 setup(
     name="ssb-ipython-kernels",
-    version="0.2.12",
+    version="0.2.13",
     url="https://github.com/statisticsnorway/dapla-ipython-kernels",
     license='MIT',
 
