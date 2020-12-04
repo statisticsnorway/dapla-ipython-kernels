d5efa68c7aabab58d5437346e3be84acb591a6e1
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2020-12-04 11:35:09 +0100

    Bump version: 0.2.16 → 0.2.17

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ DEPENDENCIES = [
 
 setup(
     name="ssb-ipython-kernels",
-    version="0.2.16",
+    version="0.2.17",
     url="https://github.com/statisticsnorway/dapla-ipython-kernels",
     license='MIT',
 
