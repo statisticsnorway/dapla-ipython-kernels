3bde8e977ba1978f134a8d73d97cec501622d5f3
RuneLind <rulind@gmail.com>
2021-01-11 15:50:43 +0100

    Bump version: 0.2.18 → 0.2.19

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ DEPENDENCIES = [
 
 setup(
     name="ssb-ipython-kernels",
-    version="0.2.18",
+    version="0.2.19",
     url="https://github.com/statisticsnorway/dapla-ipython-kernels",
     license='MIT',
 
